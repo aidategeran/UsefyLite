a1707395ce312f8a24cac2076d1c168de9a0b8ef
AidaTegeran <aidategeran@gmail.com>
2025-10-28 22:41:15 -0500

    testing

---
+++ src/test/java/com/aida/usefy_lite/service/UserServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.aida.usefy_lite.service;
+
+import com.aida.usefy_lite.dto.RegistrationRequest;
+import com.aida.usefy_lite.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void registerUser_ShouldSaveUser_WhenUsernameDoesNotExist() {
+        RegistrationRequest request = new RegistrationRequest();
+
+    }
+}
