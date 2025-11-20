1a0a871ce7386b0c9b1a918185c54a3371def997
AidaTegeran <aidategeran@gmail.com>
2025-11-19 19:51:16 -0600

    Implement a global CORS configuration

---
+++ src/main/java/com/aida/usefy_lite/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.aida.usefy_lite.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all endpoints)
+                        .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // Needed only if using cookies/auth tokens)
+
+            }
+        };
+
+    }
+}
